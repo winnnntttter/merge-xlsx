714524deff7b1085e33c9a8b56ee96c937669384
winnnntttter <1013222027@qq.com>
2020-01-21 17:04:06 +0800

    修改文件判断方法

--- extension.js
+++ extension.js
@@ -2,50 +2,56 @@ const vscode = require("vscode");
 const XLSX = require("xlsx");
 const path = require("path");
 const fs = require("fs");
-
+// var a = /\.xlsx|\.xls$/;
 function activate(context) {
   vscode.commands.registerCommand("mergeXlsx.input", function() {
     if (vscode.workspace.workspaceFolders.length === 1) {
       const basePath = vscode.workspace.workspaceFolders[0].uri.fsPath;
       vscode.window
         .showInputBox({
-          prompt: "merge xlsx: 请输入当前工作区根目录第一个xlsx文件名。"
+          prompt: "merge xlsx: Please enter the first xlsx or xls file name."
         })
         .then(function(text) {
-          fs.exists(path.join(basePath, text), function(exists) {
-            if (exists) {
+          if (!/\.xlsx|\.xls$/.test(text)) {
+            vscode.window.showErrorMessage("Please choose a xlsx or xls file!");
+            return;
+          }
+          fs.stat(path.join(basePath, text), function(err, file) {
+            if (err) {
+              vscode.window.showErrorMessage(err.message);
+              return;
+            }
+            if (file) {
               vscode.window
                 .showInputBox({
-                  prompt: "merge xlsx: 请输入当前工作区根目录第二个xlsx文件名。"
+                  prompt: "merge xlsx: Please enter the second xlsx or xls file name."
                 })
                 .then(function(text2) {
-                  fs.exists(path.join(basePath, text), function(exists2) {
-                    if (exists2) {
+                  fs.stat(path.join(basePath, text), function(err, file) {
+                    if (err) {
+                      vscode.window.showErrorMessage(err.message);
+                      return;
+                    }
+                    if (file) {
                       let workbook = XLSX.readFile(path.join(basePath, text));
-                      let workbook2 = XLSX.readFile(
-                        path.join(basePath, text2)
-                      );
+                      let workbook2 = XLSX.readFile(path.join(basePath, text2));
                       let sheetNames = workbook.SheetNames;
                       let sheetNames2 = workbook2.SheetNames;
                       let worksheet = workbook.Sheets[sheetNames[0]];
                       let worksheet2 = workbook2.Sheets[sheetNames2[0]];
                       let sheet1 = XLSX.utils.sheet_to_json(worksheet);
                       let sheet2 = XLSX.utils.sheet_to_json(worksheet2);
-                      let key1 = worksheet.A1.h;
-                      const result = sheet1
-                        .concat(sheet2)
-                        .reduce((prev, next) => {
-                          let index = prev.findIndex(
-                            (elem, i) => elem[key1] === next[key1]
-                          );
+                      let key1 = worksheet.A1 ? worksheet.A1.h : "__EMPTY";
+                      const result = sheet1.concat(sheet2).reduce((prev, next) => {
+                        let index = prev.findIndex((elem, i) => elem[key1] === next[key1]);
 
-                          if (index === -1) {
-                            return prev.concat(next);
-                          } else {
-                            prev[index] = Object.assign({}, prev[index], next);
-                            return prev;
-                          }
-                        }, []);
+                        if (index === -1) {
+                          return prev.concat(next);
+                        } else {
+                          prev[index] = Object.assign({}, prev[index], next);
+                          return prev;
+                        }
+                      }, []);
                       var wb = {
                         SheetNames: ["mySheet"],
                         Sheets: {
@@ -55,14 +61,15 @@ function activate(context) {
 
                       XLSX.writeFile(wb, path.join(basePath, `output-${new Date().getTime()}.xlsx`));
                     } else {
-                      vscode.window.showErrorMessage(
-                        `当前工作区根路径不存在${text2}!`
-                      );
+                      vscode.window.showErrorMessage(`There is no ${text2} in the workspace root folder!`);
                     }
                   });
+                })
+                .catch(function(err) {
+                  vscode.window.showErrorMessage(err);
                 });
             } else {
-              vscode.window.showErrorMessage(`当前工作区根路径不存在${text}!`);
+              vscode.window.showErrorMessage(`There is no ${text} in the workspace root folder!`);
             }
           });
         });
